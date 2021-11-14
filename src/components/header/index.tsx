84a9a6ea6959b2dcd28678bec65e2705a2954b58
RubanK <rubin@dazzlebirds.com>
2021-11-14 06:51:17 +0530

    First commit 👌

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { Center } from "@chakra-ui/react";
-
-import { Logo } from "@components";
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Button,
+  Image,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 export const Header: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Center bg="header.100">
-      <Logo />
-    </Center>
+    <>
+      <Box
+        borderBottom="1px"
+        borderColor={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        py={2}
+      >
+        <Flex
+          maxW={"2xl"}
+          h={16}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          marginX={"auto"}
+        >
+          <Box w={["80px", "120px"]}>
+            <Image src="/images/logo.png" alt="FLAMES Logo" />
+          </Box>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </Button>
+        </Flex>
+      </Box>
+    </>
   );
 };
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,21 +1,106 @@
-import React from "react";
-import { Box, useTheme } from "@chakra-ui/react";
-
-import { Button } from "@components";
+import Head from "next/head";
+import {
+  Box,
+  Heading,
+  Container,
+  VStack,
+  HStack,
+  Button,
+  IconButton,
+  Input,
+  FormLabel,
+  FormControl,
+  Image,
+  Tooltip,
+  Text,
+  AspectRatio,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { RepeatIcon } from "@chakra-ui/icons";
 
 export const Main: React.FC = () => {
-  const theme = useTheme();
   return (
-    <Box bg="main.100" color="white" textAlign="center" py={10}>
-      <h1 style={{ fontSize: theme.fontSizes["5xl"] }}>superplate</h1>
-      <p style={{ fontSize: theme.fontSizes["lg"] }}>
-        The frontend boilerplate with superpowers!
-      </p>
-      <Button colorScheme="blue" variant="solid" size="md">
-        <a href="https://pankod.github.io/superplate/" target="_blank">
-          Docs
-        </a>
-      </Button>
-    </Box>
+    <>
+      <Container maxW={"xl"} px={4}>
+        <Heading
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize={["3xl", "4xl"]}
+          marginTop={[8, 12]}
+          fontWeight="extrabold"
+          as="h1"
+          size="2xl"
+          textAlign="center"
+        >
+          Get to know about your Future
+        </Heading>
+        <Box fontFamily="monospace" mt={8}>
+          <FormControl>
+            <VStack spacing={6}>
+              <div style={{ width: "100%" }}>
+                <FormLabel htmlFor="myName">Your Name</FormLabel>
+                <Input
+                  type="text"
+                  id="myName"
+                  focusBorderColor="purple.500"
+                  placeholder="Name like rooben"
+                  aria-describedby="My-Name"
+                  size="lg"
+                />
+              </div>
+              <div style={{ width: "100%" }}>
+                <FormLabel htmlFor="yourPartner">Your Partner's Name</FormLabel>
+                <Input
+                  type="text"
+                  id="yourPartner"
+                  focusBorderColor="purple.500"
+                  placeholder="Name like NE"
+                  aria-describedby="Your-Partner"
+                  size="lg"
+                />
+              </div>
+            </VStack>
+            <HStack direction="row" spacing={4} mt={8}>
+              <Button colorScheme="gray" variant="solid" w="100%" size="lg">
+                <HStack>
+                  <Box w="24px">
+                    <Image src="/images/love_Icon.png" alt="rooben" />
+                  </Box>
+                  <Text>Check your Fate</Text>
+                </HStack>
+              </Button>
+              <Tooltip
+                label="Restart!"
+                placement="top"
+                aria-label="Restart tooltip"
+              >
+                <IconButton
+                  size="lg"
+                  variant="outline"
+                  aria-label="Reset"
+                  icon={<RepeatIcon />}
+                />
+              </Tooltip>
+            </HStack>
+          </FormControl>
+
+          <Box mt={12}>
+            <Heading as="h2" size="sm" fontFamily="monospace">
+              Result
+            </Heading>
+            <Box borderRadius="md" mt={4} mb={12}>
+              <AspectRatio ratio={1}>
+                <Image
+                  src="/images/Friends.png"
+                  width="502"
+                  height="502"
+                  alt=""
+                />
+              </AspectRatio>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 };

73e15ba5788267845ca444b916e34388668b2b91
RubanK <rubin@dazzlebirds.com>
2021-11-14 08:38:23 +0530

    added functionality

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import { useState, useEffect, useRef } from "react";
 import {
   Box,
   Heading,
@@ -17,8 +17,77 @@ import {
   FormHelperText,
 } from "@chakra-ui/react";
 import { RepeatIcon } from "@chakra-ui/icons";
+import { useToast } from "@chakra-ui/toast";
 
 export const Main: React.FC = () => {
+  const toast = useToast();
+
+  const [yourName, setYourName] = useState("");
+  const [yourPartner, setyourPartner] = useState("");
+  const [result, setResult] = useState("");
+  var flames = [
+    "Friends",
+    "Love",
+    "Affection",
+    "Marriage",
+    "Enemy",
+    "Siblings",
+  ];
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (yourName != "" && yourPartner != "") {
+      var lengthInput = yourName.length + yourPartner.length;
+      while (flames.length != 1) {
+        flames = flames.splice(lengthInput % 6, 1);
+      }
+      switch (flames[0]) {
+        case "Friends":
+          setResult("Friends");
+          break;
+        case "Love":
+          setResult("Love");
+          break;
+        case "Affection":
+          setResult("Affection");
+          break;
+        case "Marriage":
+          setResult("Marriage");
+          break;
+        case "Enemy":
+          setResult("Enemy");
+          break;
+        case "Siblings":
+          setResult("Siblings");
+          break;
+        default:
+          break;
+      }
+    }
+
+    setYourName("");
+    setyourPartner("");
+  };
+
+  useEffect(() => {
+    console.log(result);
+  });
+
+  const handleReset = () => {
+    if (yourName != "" || yourPartner != "") {
+      setYourName("");
+      setyourPartner("");
+      console.log(yourName);
+      toast({
+        title: "Your name's have been reset",
+        position: "bottom-right",
+        variant: "subtle",
+        status: "success",
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <>
       <Container maxW={"xl"} px={4}>
@@ -34,34 +103,48 @@ export const Main: React.FC = () => {
         >
           Get to know about your Future
         </Heading>
+
         <Box fontFamily="monospace" mt={8}>
-          <FormControl>
+          <form onSubmit={handleSubmit}>
             <VStack spacing={6}>
               <div style={{ width: "100%" }}>
-                <FormLabel htmlFor="myName">Your Name</FormLabel>
-                <Input
-                  type="text"
-                  id="myName"
-                  focusBorderColor="purple.500"
-                  placeholder="Name like rooben"
-                  aria-describedby="My-Name"
-                  size="lg"
-                />
+                <FormControl isRequired>
+                  <FormLabel htmlFor="myName">Your Name</FormLabel>
+                  <Input
+                    type="text"
+                    focusBorderColor="purple.500"
+                    placeholder="Name like rooben"
+                    aria-describedby="Your-Name"
+                    size="lg"
+                    onChange={(event) => setYourName(event.currentTarget.value)}
+                  />
+                </FormControl>
               </div>
               <div style={{ width: "100%" }}>
-                <FormLabel htmlFor="yourPartner">Your Partner's Name</FormLabel>
-                <Input
-                  type="text"
-                  id="yourPartner"
-                  focusBorderColor="purple.500"
-                  placeholder="Name like NE"
-                  aria-describedby="Your-Partner"
-                  size="lg"
-                />
+                <FormControl isRequired>
+                  <FormLabel htmlFor="myPartner">Your Partner's Name</FormLabel>
+                  <Input
+                    type="text"
+                    focusBorderColor="purple.500"
+                    placeholder="Name like NE"
+                    aria-describedby="My-Partner"
+                    size="lg"
+                    onChange={(event) =>
+                      setyourPartner(event.currentTarget.value)
+                    }
+                  />
+                </FormControl>
               </div>
             </VStack>
+
             <HStack direction="row" spacing={4} mt={8}>
-              <Button colorScheme="gray" variant="solid" w="100%" size="lg">
+              <Button
+                type="submit"
+                colorScheme="gray"
+                variant="solid"
+                w="100%"
+                size="lg"
+              >
                 <HStack>
                   <Box w="24px">
                     <Image src="/images/love_Icon.png" alt="rooben" />
@@ -78,27 +161,49 @@ export const Main: React.FC = () => {
                   size="lg"
                   variant="outline"
                   aria-label="Reset"
+                  onClick={() => {
+                    handleReset();
+                  }}
                   icon={<RepeatIcon />}
                 />
               </Tooltip>
             </HStack>
-          </FormControl>
+          </form>
+        </Box>
 
-          <Box mt={12}>
-            <Heading as="h2" size="sm" fontFamily="monospace">
-              Result
-            </Heading>
+        <Box mt={12}>
+          <Heading as="h2" size="sm" fontFamily="monospace">
+            Result
+          </Heading>
+          {result != "" ? (
             <Box borderRadius="md" mt={4} mb={12}>
               <AspectRatio ratio={1}>
                 <Image
-                  src="/images/Friends.png"
+                  src={`/images/${result}.png`}
                   width="502"
                   height="502"
-                  alt=""
+                  alt={`${result}`}
                 />
               </AspectRatio>
             </Box>
-          </Box>
+          ) : (
+            <Box
+              borderRadius="md"
+              border="1px"
+              borderColor="gray.200"
+              mt={4}
+              mb={12}
+            >
+              <AspectRatio ratio={1}>
+                <Box
+                  src="https://hworymbo.sirv.com/Images/Group%2010.png"
+                  width="502"
+                  height="502"
+                  alt="white image"
+                />
+              </AspectRatio>
+            </Box>
+          )}
         </Box>
       </Container>
     </>
